37a255d6a2255d798d1081e2b7cdc5bcea3274be
ugoa <hoodavy@gmail.com>
2012-07-07 11:50:08 +0800

    Just for test, my first commit.

---
+++ myfirstRBprog.rb
@@ -0,0 +1,10 @@
+#! This a ruby source file.
+puts "Hello, world"
+
+def say_goodnight(name)
+	result = "Good night, " + name
+	return result
+end
+
+puts say_goodnight("John Boy")
+puts say_goodnight("Mary Ellen")
---
+++ new.rb
@@ -0,0 +1,23 @@
+class Song
+	@@plays = 0
+	def initialize(name, artist, duration)
+		@name = name
+		@artist = artist
+		@duration = duration
+		@plays = 0
+	end
+
+	def PlaySong
+		@plays += 1
+		@@plays += 1
+		puts "THis song: #@plays times. Total #@@plays times."
+	end
+end
+
+s1 = Song.new("Song1", "Artist1", 234)
+s2 = Song.new("Song2", "Artist2", 236)
+
+s1.PlaySong	
+s2.PlaySong	
+s1.PlaySong	
+s1.PlaySong	
