601a03061a37f85e5400445628af7aba8c31fd29
David Hu <hoodavy@gmail.com>
2012-08-19 15:39:15 +0800

    Quiz: Checked Attributes(Step 4)

--- RubySnippets/MetaprogrammingRuby/CodeWritesCode/checked_attributes/eval.rb
+++ RubySnippets/MetaprogrammingRuby/CodeWritesCode/checked_attributes/eval.rb
@@ -1,10 +1,26 @@
 require "test/unit"
 
-class Person; end
+class Class
+  def attr_checked(attribute, &validation)
+    define_method "#{attribute}=" do |value|
+      raise 'Invalid attribute' unless yield(value)
+      instance_variable_set("@#{attribute}", value)
+    end
+
+    define_method attribute do
+      instance_variable_get "@#{attribute}"
+    end
+  end
+end
+
+class Person
+  attr_checked :age do |v|
+    v >= 18
+  end
+end
 
 class TestCheckedAttributes < Test::Unit::TestCase
   def setup
-    add_checked_attributes(Person, :age) { |v| v >= 18 }
     @bob = Person.new
   end
 
@@ -18,33 +34,6 @@ class TestCheckedAttributes < Test::Unit::TestCase
       @bob.age = 17
     end
   end
-
-#   def add_checked_attributes(clazz, attribute)
-#     eval <<B
-# class #{clazz}
-#   def #{attribute}=(value)
-#     raise 'Invalid attribute' unless value
-#     @#{attribute} = value
-#   end
-
-#   def #{attribute}()
-#     @#{attribute}
-#   end
-# end
-# B
-#   end
-
-# another way to generate reader and writer method
-  def add_checked_attributes(clazz, attribute, &validation)
-    clazz.class_eval do
-      define_method "#{attribute}=" do |value|
-        raise 'Invalid attribute' unless yield(value)
-        instance_variable_set("@#{attribute}", value)
-      end
-
-      define_method attribute do
-        instance_variable_get "@#{attribute}"
-      end
-    end
-  end
 end
+
+
